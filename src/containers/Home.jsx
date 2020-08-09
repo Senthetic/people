40e84011361ceae99799dc21620ee0bec7a18053
Andrew Collier <atc3598@icloud.com>
2020-08-08 20:51:09 -0400

    added filter method

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import Employees from "../employees.json";
 class Home extends Component {
   state = {
-    employees:  Employees ,
+      search: "",
+    employees: Employees,
   };
 
-//   componentDidMount() {
-//     this.sortEmployees();
-//   }
+  //   componentDidMount() {
+  //     this.sortEmployees();
+  //   }
 
   sortEmployees = () => {
     function compare(a, b) {
@@ -23,34 +24,72 @@ class Home extends Component {
     });
   };
 
+  handleInputChange = (e) => {
+    const {name, value} = e.target;
+    this.setState({
+        [name]: value,
+    });
+  };
+  handleSubmit = (e) => {
+      e.preventDefault();
+      this.filterEmployees();
+  }
+
+  filterEmployees = () => {
+      const searchTerm = this.state.search.toLowerCase();
+      this.setState({
+          employees: this.state.employees.filter((employee) => 
+      employee.name.toLowerCase().includes(searchTerm)
+      ),
+      });
+  };
+
+
   render() {
     return (
       <div className="Container">
         <div className="row">
-        <table className="table">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col" onClick={this.sortEmployees}>
-                Name
-              </th>
-              <th scope="col">Salary</th>
-              <th scope="col">Age</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.employees.map((employee) => (
+            <div className="col">
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Search by name"
+                    name="search"
+                    value={this.state.search}
+                    onChange={this.handleInputChange}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+          <table className="table">
+            <thead className="thead-dark">
               <tr>
-                <th scope="row">{employee.id}</th>
-                <td>{employee.name}</td>
-                <td>{employee.salary}</td>
-                <td>{employee.age}</td>
+                <th scope="col">ID</th>
+                <th scope="col" onClick={this.sortEmployees}>
+                  Name
+                </th>
+                <th scope="col">Salary</th>
+                <th scope="col">Age</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      </div>
+            </thead>
+            <tbody>
+              {this.state.employees.map((employee) => (
+                <tr>
+                  <th scope="row">{employee.id}</th>
+                  <td>{employee.name}</td>
+                  <td>{employee.salary}</td>
+                  <td>{employee.age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
     );
   }
 }
